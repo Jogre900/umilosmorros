fedbdd9da70d9542c38d48114c04d55861877e78
jogre900 <josekasike@gmail.com>
2019-09-30 13:41:42 -0430

    cambios generales, menu pegajoso

--- js/main.js
+++ js/main.js
@@ -4,6 +4,20 @@ function alerta(){
     let navbar = document.getElementById('nav-bar');
     navbar.classList.toggle('nav-bar-open');
 }
+//STICKY
+const menu = document.getElementById('nav-bar');
+const altura = menu.offsetTop;
+    
+    function fixedNav(){
+        if(window.scrollY >= altura){
+            document.body.style.paddingTop = menu.offsetHeight +'px'; 
+            document.body.classList.add('fixed-nav');
+        }else{
+            document.body.style.paddingTop = 0;
+            document.body.classList.remove('fixed-nav')
+        }
+    }
+    window.addEventListener('scroll', fixedNav);
 
 //--SLIDER--
 var item = 0;
@@ -14,6 +28,7 @@ rigth.addEventListener('click', () => {changeBox(1)});
 left.addEventListener('click', () => {changeBox(2)});
 
 const sliderBox = document.querySelectorAll('.slider-box');
+
 function changeBox(n, silderBox){
     if(n == 1){
         if(item == 2){
